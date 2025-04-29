511ffd1a5f565eadf628597642df9a67b7615646
Otavio <estudosprogotavio@gmail.com>
2025-04-29 15:50:30 -0300

    Removendo Lógica de alternancia de tema em FinanceApp.js

--- src/componentes/Templete/index.js
+++ src/componentes/Templete/index.js
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import './Template.css';
-import React, { useState, useEffect, useContext } from 'react';
-import { TemaContext } from '../TemaContext';
+import React, { useState, useEffect } from 'react';
 import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
 ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
@@ -16,7 +15,6 @@ const FinanceApp = ({ usuario }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [transacaoEditando, setTransacaoEditando] = useState(null);
   const navigate = useNavigate();
-  const { temaEscuro, toggleTema } = useContext(TemaContext); // Obtendo o contexto do tema
 
   useEffect(() => {
     localStorage.setItem('transacoes', JSON.stringify(transacoes));
@@ -69,19 +67,19 @@ const FinanceApp = ({ usuario }) => {
   };
 
   return (
-    <div className={`finance-container ${temaEscuro ? 'tema-escuro' : 'tema-claro'}`}>
-      <div className={`finance-box ${temaEscuro ? 'finance-box-escuro' : 'finance-box-claro'}`}>
+    <div className="finance-container">
+      <div className="finance-box">
         <h1>Bem-vindo, {usuario.nome || 'Usuário'}</h1>
         <p className="saldo-total">Saldo: R$ {saldoTotal.toFixed(2)}</p>
 
-        <div className={`transacao-form ${temaEscuro ? 'transacao-form-escuro' : 'transacao-form-claro'}`}>
+        <div className="transacao-form">
           <input
             type="text"
             placeholder="Descrição"
             value={descricao}
             onChange={(e) => setDescricao(e.target.value)}
             disabled={isEditing && !transacaoEditando}
-            className={temaEscuro ? 'input-escuro' : 'input-claro'}
+            className="input-claro"
           />
           <input
             type="number"
@@ -89,20 +87,20 @@ const FinanceApp = ({ usuario }) => {
             value={valor}
             onChange={(e) => setValor(e.target.value)}
             disabled={isEditing && !transacaoEditando}
-            className={temaEscuro ? 'input-escuro' : 'input-claro'}
+            className="input-claro"
           />
           <select
             value={tipo}
             onChange={(e) => setTipo(e.target.value)}
             disabled={isEditing && !transacaoEditando}
-            className={temaEscuro ? 'input-escuro' : 'input-claro'}
+            className="input-claro"
           >
             <option value="receita">Receita</option>
             <option value="despesa">Despesa</option>
           </select>
           <button
             onClick={isEditing ? editarTransacao : adicionarTransacao}
-            className={`btn-add ${temaEscuro ? 'btn-escuro' : 'btn-claro'}`}
+            className="btn-claro"
           >
             {isEditing ? 'Salvar Edição' : 'Adicionar'}
           </button>
@@ -111,22 +109,21 @@ const FinanceApp = ({ usuario }) => {
         <div className="buttons">
           <button
             onClick={() => setIsEditing(!isEditing)}
-            className={`btn-edit ${temaEscuro ? 'btn-escuro' : 'btn-claro'}`}
+            className="btn-claro"
           >
             {isEditing ? 'Cancelar Edição' : 'Editar Transações'}
           </button>
           <button
             onClick={irParaGraficos}
-            className={`btn-graficos ${temaEscuro ? 'btn-escuro' : 'btn-claro'}`}
+            className="btn-claro"
           >
             Ver Gráficos
           </button>
-          
         </div>
 
         <div className="transacoes-list">
           <h3>Transações</h3>
-          <table className={`tabela-transacoes ${temaEscuro ? 'tabela-escuro' : 'tabela-claro'}`}>
+          <table className="tabela-claro">
             <thead>
               <tr>
                 <th>Descrição</th>
@@ -144,13 +141,13 @@ const FinanceApp = ({ usuario }) => {
                   <td>
                     <button
                       onClick={() => excluirTransacao(transacao.id)}
-                      className={`btn-delete ${temaEscuro ? 'btn-escuro' : 'btn-claro'}`}
+                      className="btn-claro"
                     >
                       Excluir
                     </button>
                     <button
                       onClick={() => iniciarEdicao(transacao)}
-                      className={`btn-edit-transacao ${temaEscuro ? 'btn-escuro' : 'btn-claro'}`}
+                      className="btn-claro"
                     >
                       Editar
                     </button>
