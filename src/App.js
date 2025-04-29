d4e2b440cf47c8ed616a04626e07bdbb69ff69ed
Otavio <estudosprogotavio@gmail.com>
2025-04-29 15:46:41 -0300

    Retirando Lógica de alternancia de tema de App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
-import React, { useState, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './componentes/TelaLogin';
 import Cadastro from './componentes/TelaCadastro';
 import FinanceApp from './componentes/Templete';
 import GraficoPage from './componentes/Graficos/';
 import './App.css';
-import { TemaProvider, TemaContext } from './componentes/TemaContext';
+import React, { useState } from 'react';  // Importando React e useState
 
 function App() {
   return (
-    <TemaProvider>
-      <Router>
-        <ConteudoApp />
-      </Router>
-    </TemaProvider>
+    <Router>
+      <ConteudoApp />
+    </Router>
   );
 }
 
 function ConteudoApp() {
   const [usuarioLogado, setUsuarioLogado] = useState(null);
-  const { temaEscuro, toggleTema } = useContext(TemaContext); 
 
   return (
-    <div className={temaEscuro ? 'tema-escuro' : 'tema-claro'}>
+    <div className="tema-claro"> {/* Aplicando apenas o tema claro */}
       <Routes>
         <Route path="/" element={<Login setUsuarioLogado={setUsuarioLogado} />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/home" element={<FinanceApp usuario={usuarioLogado} />} />
         <Route path="/grafico" element={<GraficoPage />} />
       </Routes>
-
-    
-      <div className="container-botao">
-        <button onClick={toggleTema} className="btn-alternar-tema">
-          {temaEscuro ? 'Modo Claro' : 'Modo Escuro'}
-        </button>
-      </div>
     </div>
   );
 }
