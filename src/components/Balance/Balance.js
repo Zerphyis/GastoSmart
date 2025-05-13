387305c2a12a5b8cf8ff4928a68e50a4f2f47873
Otavio <estudosprogotavio@gmail.com>
2025-05-12 23:21:18 -0300

    Feat:adicionando Balance.js

---
+++ src/components/Balance/Balance.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Balance.css'; // Importando o CSS
+
+export default function Balance({ transactions }) {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+  const isPositive = Number.parseFloat(total) >= 0;
+
+  return (
+    <div className="balance-container">
+      <h4 className="balance-title">Seu Saldo</h4>
+      <h1 className={`balance-amount ${isPositive ? "positive" : "negative"}`}>${total}</h1>
+    </div>
+  );
+}
