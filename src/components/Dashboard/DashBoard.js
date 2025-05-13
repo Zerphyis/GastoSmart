6decef3b791d09cf9ab09e3fbb146222e1c674c2
Otavio <estudosprogotavio@gmail.com>
2025-05-12 23:21:43 -0300

    Feat:adicionando DashBoard.js

---
+++ src/components/Dashboard/DashBoard.js
@@ -0,0 +1,72 @@
+
+import { useState } from "react";
+import Header from "../Header/Header";
+import Balance from "../Balance/Balance";
+import IncomeExpenses from "../IncomeExpenses/IncomeExpenses";
+import TransactionList from "../Transations-List/Transations-List";
+import AddTransaction from "../Add-Transations/AddTransations";
+import Analytics from "../Analics/Analics";
+import { LogOut, PieChart, History, Plus } from "lucide-react";
+import './DashBoard.css'; 
+export default function Dashboard({
+  user,
+  transactions,
+  onAddTransaction,
+  onDeleteTransaction,
+  onLogout,
+}) {
+  const [activeTab, setActiveTab] = useState("transactions");
+
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-inner-container">
+        <div className="dashboard-header">
+          <Header userName={user.name} />
+          <button onClick={onLogout} className="logout-button">
+            <LogOut size={16} />
+            <span>Sair da Conta</span>
+          </button>
+        </div>
+
+        <div className="dashboard-grid">
+          <Balance transactions={transactions} />
+          <IncomeExpenses transactions={transactions} />
+        </div>
+
+        <div className="dashboard-tabs-container">
+          <div className="tabs">
+            <button
+              className={`tab ${activeTab === "transactions" ? "active" : ""}`}
+              onClick={() => setActiveTab("transactions")}
+            >
+              <History size={16} className="icon" />
+              <span>Transações</span>
+            </button>
+            <button
+              className={`tab ${activeTab === "analytics" ? "active" : ""}`}
+              onClick={() => setActiveTab("analytics")}
+            >
+              <PieChart size={16} className="icon" />
+              <span>Análise</span>
+            </button>
+            <button
+              className={`tab ${activeTab === "add" ? "active" : ""}`}
+              onClick={() => setActiveTab("add")}
+            >
+              <Plus size={16} className="icon" />
+              <span>Adicionar Nova</span>
+            </button>
+          </div>
+
+          <div className="tabs-content">
+            {activeTab === "transactions" && (
+              <TransactionList transactions={transactions} onDeleteTransaction={onDeleteTransaction} />
+            )}
+            {activeTab === "analytics" && <Analytics transactions={transactions} />}
+            {activeTab === "add" && <AddTransaction onAddTransaction={onAddTransaction} />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
